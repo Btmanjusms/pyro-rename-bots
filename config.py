105bd4c59cb079bd3379775bbaf0bb102aac1dc6
Btmanjusms <90103484+Btmanjusms@users.noreply.github.com>
2023-01-12 21:59:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "8813038")
+API_ID = os.environ.get("API_ID", "977080")
 
-API_HASH = os.environ.get("API_HASH", "780fd96b159baa710dada78ff1621b54")
+API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5566607544:AAHclutbOHgm3Bvo28XRpn2IUDzSxuSWnl8") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5743083943:AAEZz749XcKTM7Mfb0srymLseTcw5rcCPnk") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "") 
 
